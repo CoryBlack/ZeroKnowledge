cd075c089402ec14b5a6b9e442ee724bde511a71
Natalie Andreeva <andren4@cs.washington.edu>
2017-05-05 15:53:29 +0200

    server stubbs filled

--- ZKPServer.py
+++ ZKPServer.py
@@ -3,7 +3,7 @@ import socket
 import random as rnd
 
 HOST = ''
-PORT = 50008
+PORT = 50000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -11,20 +11,17 @@ conn, addr = s.accept()
 print 'Connected by', addr
 
 #-------------------------------------------------------------------
-#Our code below, rest is just boilerplate connections
+# client sends true or false for if we are registering
+register = conn.recv(1024)
+if (register == true)
+	username = conn.recv(1024)
+	Y = conn.recv(1024)
+	usernames[username] = Y
 
 rnd.seed(1) #just for tests
 usernames = {}
-Ys = {}
 n = 13
 g = 5
-
-#REGISTRATION
-#If connected for registration, wait for the username and Y and store it
-usernames[username] = len(usernames)
-Ys[len(Ys)] = Y
-
-
 
 
 #Begin Authentication
@@ -32,27 +29,18 @@ Ys[len(Ys)] = Y
 A = generateA()
 conn.sendall(A)
 
-#Receives c and z as a result of the client calculations
-#username = ...
-#c = ...
-#z = ...
+#TODO: change the buffer size here ?
+c = int(conn.recv(8192))
+z = int(conn.recv(8192))
 t = generateT()
 
-zPrime = hash(str(y) + str(t) + str(A))
+# server calculates its expected c
+cPrime = hash(str(y) + str(t) + str(A))
 
+# if match, then client is authenticated
 if c == cPrime:
-	#user has correctly authenticated
 	conn.sendall("You're in!")
 
-
-#-------------------------------------------------------------------------------------
-#Boilerplate input output code
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-conn.close()
-#-------------------------------------------------------------------------------------
 
 def generateA():
 	return rnd.randint(0,10000) #just a placeholder
@@ -61,7 +49,6 @@ def generateA():
 #look up the y corresponding to the username
 def generateT(username, c, z):
 	if usernames.has_key(username):
-                key = usernames.get(username)
-                y = Ys.get(key)
+                y = usernames.get(username)
 	# We dont know if Mod is necessary or not TODO
 	return ((y ** c) * (g ** z)) % n
