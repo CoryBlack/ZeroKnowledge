e8e2e1dfb16ed2a5ceb8bf946357ad0b4795b617
Natalie Andreeva <andren4@cs.washington.edu>
2017-05-05 16:20:59 +0200

    server now branched to expect different actions; refactoring

--- ZKPServer.py
+++ ZKPServer.py
@@ -15,45 +15,47 @@ usernames = {}
 n = 13
 g = 5
 
-#-------------------------------------------------------------------
-# client sends true or false for if we are registering
-# loop continues until false is sent - meaning we want to authenticate
-while 1:
-	register = conn.recv(1024)
-	if (register == "true"):
-		username = conn.recv(1024)
-		Y = conn.recv(1024)
-		usernames[username] = Y
-	else:
-		break
-
-
-
 def generateA():
 	return rnd.randint(0,10000) #just a placeholder
 
 
 #look up the y corresponding to the username
-def generateT(username, c, z):
-	if usernames.has_key(username):
-                y = usernames.get(username)
+def generateT(y, c, z):
 	# We dont know if Mod is necessary or not TODO
 	return ((y ** c) * (g ** z)) % n
 
 
-#Begin Authentication
-#Generate integer a
-A = generateA()
-conn.sendall(str(A))
+def authenticate():
+	A = generateA()
+	conn.sendall(str(A))
 
-#TODO: change the buffer size here ?
-# c = int(conn.recv(8192))
-# z = int(conn.recv(8192))
-t = generateT()
+	#TODO: change the buffer size here ?
+	# server receives the username, c, and z
+	curr_username = conn.recv(1024)
+	c = int(conn.recv(8192))
+	z = int(conn.recv(8192))
+	y = usernames[curr_username]
 
-# server calculates its expected c
-cPrime = hash(str(y) + str(t) + str(A))
+	t = generateT(y, c, z)
+	# server calculates its expected c
+	cPrime = hash(str(y) + str(t) + str(A))
 
-# if match, then client is authenticated
-if c == cPrime:
-	conn.sendall("You're in!")
+	# if match, then client is authenticated
+	if c == cPrime:
+		conn.sendall("You're in!")
+
+
+#-------------------------------------------------------------------
+# client sends true or false for if we are registering
+# loop continues until false is sent - meaning we want to authenticate
+while 1:
+	action = conn.recv(1024)
+	if (action == "register"):
+		username = conn.recv(1024)
+		Y = conn.recv(1024)
+		usernames[username] = Y
+	else if (action == "authenticate"):
+		# want to authenticate
+		authenticate()
+	else:
+		break
