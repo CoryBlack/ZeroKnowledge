5c6383c9a13ca5b6b9d6f377d59237f42194a232
Natalie Andreeva <andren4@cs.washington.edu>
2017-05-05 16:07:21 +0200

    syntax

--- ZKPServer.py
+++ ZKPServer.py
@@ -3,7 +3,7 @@ import socket
 import random as rnd
 
 HOST = ''
-PORT = 50000
+PORT = 50055
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -19,32 +19,13 @@ while 1:
 		username = conn.recv(1024)
 		Y = conn.recv(1024)
 		usernames[username] = Y
-	else
+	else:
 		break
 
 rnd.seed(1) #just for tests
 usernames = {}
 n = 13
 g = 5
-
-
-#Begin Authentication
-#Generate integer a
-A = generateA()
-conn.sendall(A)
-
-#TODO: change the buffer size here ?
-c = int(conn.recv(8192))
-z = int(conn.recv(8192))
-t = generateT()
-
-# server calculates its expected c
-cPrime = hash(str(y) + str(t) + str(A))
-
-# if match, then client is authenticated
-if c == cPrime:
-	conn.sendall("You're in!")
-
 
 def generateA():
 	return rnd.randint(0,10000) #just a placeholder
@@ -56,3 +37,21 @@ def generateT(username, c, z):
                 y = usernames.get(username)
 	# We dont know if Mod is necessary or not TODO
 	return ((y ** c) * (g ** z)) % n
+
+
+#Begin Authentication
+#Generate integer a
+A = generateA()
+conn.sendall(str(A))
+
+#TODO: change the buffer size here ?
+# c = int(conn.recv(8192))
+# z = int(conn.recv(8192))
+t = generateT()
+
+# server calculates its expected c
+cPrime = hash(str(y) + str(t) + str(A))
+
+# if match, then client is authenticated
+if c == cPrime:
+	conn.sendall("You're in!")
